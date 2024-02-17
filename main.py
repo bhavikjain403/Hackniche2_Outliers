7c73b2d01d60a059c5a648912faee38e9f2f3698
PrachetShah <prachetshah25@gmail.com>
2024-02-17 14:25:50 +0530

    menu extraction complete

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, jsonify
 from voiceExtraction import extract_order_spacy_ner
+from menuExtraction import menuExtract
+from werkzeug.utils import secure_filename
+import requests
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.getcwd()+'/submitted'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
@@ -35,5 +43,17 @@ def orderExtraction():
         }
         return jsonify(output)
 
+
+@app.route("/admin/menuextract", methods=["POST"])
+def menuExtraction():
+    if request.method=="POST":
+        img = request.files['image']
+
+        img_loc = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename))
+        img.save(img_loc)
+
+        return menuExtract(img_loc)
+    else:
+        return jsonify("Invalid Method")
 
 app.run(debug=True)
---
+++ menuExtraction.py
@@ -0,0 +1,35 @@
+import requests
+
+def menuExtract(img_loc):
+    headers = {
+        "apikey":"K82390265288957",
+        "filetype": "JPG",
+        "isTable": "true"
+    }
+    payload = {
+        "isTable": "true"
+    }
+    output = {
+        "success": True,
+        "msg": "",
+        "data": []
+    }
+    try:
+        with open(img_loc, 'rb') as f:
+            response = requests.post("https://api.ocr.space/parse/image", headers=headers, data=payload, files={"image": f}).json()
+            data = response["ParsedResults"][0]["ParsedText"].split("\t\r")
+            # print(data)
+            menu = []
+            for items in data:
+                if "\t" in items:
+                    val = {}
+                    dish, price = items.split("\t")
+                    val["dish"] = dish[1:]
+                    val["price"] = price 
+                    menu.append(val)
+            print(menu)            
+        return menu
+    except Exception as e:
+        output["success"] = False 
+        output["msg"] = str(e) 
+        return output
