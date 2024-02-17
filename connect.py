876246aa18a3c42c94d8f3ca66cbc88a7fd0dae6
PrachetShah <prachetshah25@gmail.com>
2024-02-17 18:12:30 +0530

    wa bot try

---
+++ connect.py
@@ -0,0 +1,9 @@
+# from flask_pymongo import pymongo
+# from main import app as app
+
+# CONNECTION_STRING = "mongodb+srv://bhavikjain403:3JXLUL5N1pzApsyb@cluster0.1deu8rv.mongodb.net/?retryWrites=true&w=majority"
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('test')
+# user_collection = pymongo.collection.Collection(db, 'users')
+# menu_collection = pymongo.collection.Collection(db, 'menus')
+# orders_collection = pymongo.collection.Collection(db, 'orders')
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from flask import Flask, request, jsonify
 from voiceExtraction import extract_order_spacy_ner
 from menuExtraction import menuExtract
+
 from werkzeug.utils import secure_filename
-import requests
+
+from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.rest import Client
+# from connect import db as db
+from bson.json_util import dumps
+
+from flask_cors import CORS
 import os
+
+from flask_pymongo import pymongo
+
 from textblob import TextBlob
+from decouple import config
 
 app = Flask(__name__)
+CORS(app)
+
+CONNECTION_STRING = "mongodb+srv://bhavikjain403:3JXLUL5N1pzApsyb@cluster0.1deu8rv.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('test')
+user_collection = pymongo.collection.Collection(db, 'users')
+menu_collection = pymongo.collection.Collection(db, 'menus')
+orders_collection = pymongo.collection.Collection(db, 'orders')
+
+SID = config("TWILIO_ACCOUNT_SID")
+Auth = config("TWILIO_AUTH_TOKEN")
+
+client = Client(SID, Auth)
+# Bhavikjain@170810
+def respond(text, img):
+    response = MessagingResponse()
+    message = Message()
+    message.body(text)
+    message.media(img)
+    return str(response.append(message))
 
 UPLOAD_FOLDER = os.getcwd()+'/submitted'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -15,6 +46,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/reply', methods=['POST'])
+def reply():
+    message = request.form.get('Body').lower()
+    num=request.form.get('From')[12:]
+    img = request.form["img"]
+    data=list(db.user_collection.find({ "phone" : num }))
+    print(data)
+    if len(data):
+        return respond("Today's special is XYZ Food", img)
+    else:
+        return respond('You do not have an account. Please create an account first')
 
 @app.route("/extract", methods=["POST"])
 def orderExtraction():
